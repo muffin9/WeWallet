b07acdc038c49bb3c21997691a5bfa4975c89357
muffin9 <jinlog9@gmail.com>
2023-06-17 23:55:04 +0900

    refac: styeld-components remove file

--- FE/src/styles/GlobalStyles.ts
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-import { normalize } from "styled-normalize";
-
-const GlobalStyles = createGlobalStyle`
-    ${normalize}
-`;
-
-export default GlobalStyles;
