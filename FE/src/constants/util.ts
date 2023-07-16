c5e53377e41f85a1f303cf88e9910473e5418cff
sungjin.kim@voithru.com <sungjin.kim@voithru.com>
2023-07-16 20:32:00 +0900

    ♻️: [FE] Avatar Compoennt refactor

---
+++ FE/src/constants/util.ts
@@ -0,0 +1,2 @@
+export const SampleImageUrl =
+  'https://i.namu.wiki/i/fhAwG44l_CoHhU1RJzn-gaonLHYVCChkgbqm2MG7V3F5U4B9jGpGPbLDM2vTtVRe8sRv9D-I8I5_V3GI5wPi6qOxJ4XhNXSeI7L-26tEHfZko1hxf17RLtkNWbZ39CF2eF_XcvWx3M0myx4BbZ6abA.webp';
