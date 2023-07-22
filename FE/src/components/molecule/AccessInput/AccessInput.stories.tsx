993a4c61e3423c50b01dfda4b6dbf415e687d628
sungjin.kim@voithru.com <sungjin.kim@voithru.com>
2023-07-22 16:01:06 +0900

    🚧: [FE] AccessInput Component complete.

---
+++ FE/src/components/molecule/AccessInput/AccessInput.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta } from '@storybook/react';
+import AccessInput from '.';
+import Input from '@/components/atoms/Input';
+import { useState } from 'react';
+
+const meta: Meta<typeof AccessInput> = {
+  title: 'Primitive/AccessInput',
+  component: AccessInput,
+};
+
+export default meta;
+
+const PrimitiveAccessInput = () => {
+  const [email, setEmail] = useState('');
+  return (
+    <AccessInput
+      variant="default"
+      InputComponent={
+        <Input
+          type="text"
+          variant="default"
+          placeholder="이메일을 입력해주세요."
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+          value={email}
+        />
+      }
+      onResetValue={() => setEmail('')}
+    />
+  );
+};
+
+export const Primary = {
+  render: () => <PrimitiveAccessInput />,
+};
