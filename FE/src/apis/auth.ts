1e541cfcddebb207d55ad63808aa40c39a74b211
Muffin <jinha6707@gmail.com>
2023-07-23 12:47:59 +0900

    🚧 Signup Page UI, react-hook-form apply ing...

--- FE/src/apis/auth.ts
+++ FE/src/apis/auth.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+export const checkDupliateEmail = async (email: string) => {
+  try {
+    const response = await axios.get(
+      `${process.env.API_URL}/user/check?email=${email}`,
+    );
+
+    return response.data;
+  } catch (err) {
+    throw new Error('check duplicate email error');
+  }
+};
+
 export const kakaoAuth = async (code: string | string[]) => {
   try {
     const response = await axios.post(`${process.env.API_URL}/auth/kakao`, {
