675caa7fc2489741088349e2456f4bcbb4c65729
Muffin <jinha6707@gmail.com>
2023-07-23 23:58:08 +0900

    🚧 [BE] Local Login ing... create signup db data save, token set ing.

---
+++ BE/src/auth/interface/local.user.ts
@@ -0,0 +1,7 @@
+export type LocalUserTypeResponse = {
+  email: string;
+  name: string;
+  nickname: string;
+  accessToken: string;
+  refreshToken: string;
+};
---
+++ BE/src/user/interface/status.ts
@@ -0,0 +1,3 @@
+export const USER_STATUS = {
+  USER_CREATED: 'USER_CREATED',
+} as const;
