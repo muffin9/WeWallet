6a0fb5b97632a869c023596aee5d1984c668226f
sungjin.kim@voithru.com <sungjin.kim@voithru.com>
2023-07-23 15:50:42 +0900

    🚧 [FE] Modal Component apply.

--- FE/src/components/molecule/Modal/Modal.stories.tsx
+++ FE/src/components/molecule/Modal/Modal.stories.tsx
@@ -11,12 +11,7 @@ export default meta;
 
 const PrimitiveModal = () => {
   return (
-    <Modal
-      title="이메일을 확인해 주세요!"
-      description="해당 이메일로 등록된 회원이 없어요."
-      size="medium"
-      handleClose={() => console.log('closed popup')}
-    >
+    <Modal size="medium">
       <div className="mt-auto">
         <Button
           variant="success"
