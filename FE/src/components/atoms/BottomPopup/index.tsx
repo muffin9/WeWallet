7f53b67246b76fa5b13da0b371fd73adb98b5136
Muffin <jinha6707@gmail.com>
2023-07-29 17:14:30 +0900

    FE refac atom component merge to FE
    * BE refac login merge to BE 
    
    * 🚧 [FE] add package - shadcn ui, Calendar component custom apply.
    
    * ♻️ [BE] login refac -> set token,  kakao login -> res.setHeader remove
    
    ---------
    
    * ♻️ [FE] BottomPopup, Input, Component add props / add ScrollArea Component
    
    ---------

--- FE/src/components/atoms/BottomPopup/index.tsx
+++ FE/src/components/atoms/BottomPopup/index.tsx
@@ -1,16 +1,19 @@
 interface BottomPopupProps {
   children: React.ReactNode;
   closePopup: () => void;
+  className?: string;
 }
 
-const BottomPopup = ({ children, closePopup }: BottomPopupProps) => {
+const BottomPopup = ({ children, closePopup, className }: BottomPopupProps) => {
   return (
     <>
       <div
         className="overlay"
         onClick={closePopup}
       />
-      <div className="w-full p-8 fixed left-0 bottom-0 flex justify-center bg-white border-solid border-[1px] border-success rounded-2xl z-[1000]">
+      <div
+        className={`${className} w-full fixed left-0 bottom-0 flex justify-center bg-white border-solid border-[1px] border-silver rounded-2xl z-[1000]`}
+      >
         {children}
       </div>
     </>
---
+++ FE/src/components/atoms/ScrollArea/index.tsx
@@ -0,0 +1,45 @@
+import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area';
+
+interface ScrollAreaProps {
+  title: string;
+  values: { [key: string]: string };
+  onClick: (value: string) => void;
+}
+
+const ScrollArea = ({ title, values, onClick }: ScrollAreaProps) => {
+  return (
+    <ScrollAreaPrimitive.Root className="w-full h-full rounded overflow-hidden shadow-[0_2px_10px] bg-white">
+      <ScrollAreaPrimitive.Viewport className="w-full h-full rounded">
+        <div className="py-[15px] px-5">
+          <div className="text-violet11 text-[15px] leading-[18px] font-medium">
+            {title}
+          </div>
+          {Object.entries(values).map(([key, value]) => (
+            <div
+              className="text-mauve12 text-[13px] leading-[18px] mt-2.5 pt-2.5 border-t border-t-mauve6 cursor-pointer"
+              key={key}
+              onClick={() => onClick(key)}
+            >
+              {value}
+            </div>
+          ))}
+        </div>
+      </ScrollAreaPrimitive.Viewport>
+      <ScrollAreaPrimitive.Scrollbar
+        className="flex select-none touch-none p-0.5 bg-blackA6 transition-colors duration-[160ms] ease-out hover:bg-blackA8 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
+        orientation="vertical"
+      >
+        <ScrollAreaPrimitive.Thumb className="flex-1 bg-mauve10 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
+      </ScrollAreaPrimitive.Scrollbar>
+      <ScrollAreaPrimitive.Scrollbar
+        className="flex select-none touch-none p-0.5 bg-blackA6 transition-colors duration-[160ms] ease-out hover:bg-blackA8 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
+        orientation="horizontal"
+      >
+        <ScrollAreaPrimitive.Thumb className="flex-1 bg-mauve10 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
+      </ScrollAreaPrimitive.Scrollbar>
+      <ScrollAreaPrimitive.Corner className="bg-blackA8" />
+    </ScrollAreaPrimitive.Root>
+  );
+};
+
+export default ScrollArea;
