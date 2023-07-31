01403c3df8522ba649532fcda734c9da1ab86d9a
sungjin.kim@voithru.com <sungjin.kim@voithru.com>
2023-07-31 18:26:14 +0900

    🚧 [BE] static file apply. (category image files set)

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
 
 async function main() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
@@ -16,6 +18,8 @@ async function main() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // 모든 라우터에 대해 유효성 검사가 적용되며, 검증된 데이터만 컨트롤러로 전달되도록 설정
 
   initializeTransactionalContext();
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const config = new DocumentBuilder()
     .setTitle('Wewallet API DOCS')
