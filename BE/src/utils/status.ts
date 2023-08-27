2e98a8a9b868936786a8c6b5a60e5edd8d8080e7
Muffin <jinha6707@gmail.com>
2023-08-27 14:56:01 +0900

    [BE] refac signup api - add duplicate check email logic (#87)

--- BE/src/utils/status.ts
+++ BE/src/utils/status.ts
@@ -9,6 +9,7 @@ export const TRANS_STATUS = {
 export const USER_STATUS = {
   USER_CREATED: 'U-C-200',
   USER_NONE_EMAIL: 'U-N-400',
+  USER_DUPLICATE_EMAIL: 'U-D-400',
   USER_MISMATCH_PASSWORD: 'U-M-400',
   USER_LOGIN_SUCCESS: 'U-L-200',
 } as const;
